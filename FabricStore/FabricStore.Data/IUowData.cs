c3b299eed6908fdbaedfa4d4fbae7f3729dabec7
simoto <sime679@gmail.com>
2014-11-09 14:52:28 +0200

    Added Unit of Work

---
+++ FabricStore/FabricStore.Data/IUowData.cs
@@ -0,0 +1,19 @@
+﻿namespace FabricStore.Data
+{
+    using FabricStore.Models;
+
+    public interface IUowData
+    {
+        IRepository<Product> Products { get; }
+
+        IRepository<Category> Categories { get; }
+
+        IRepository<Manufacturer> Manufacturers { get; }
+
+        IRepository<Comment> Comments { get; }
+
+        IRepository<Tag> Tags { get; }
+
+        int SaveChanges();
+    }
+}
---
+++ FabricStore/FabricStore.Data/UowData.cs
@@ -0,0 +1,70 @@
+﻿namespace FabricStore.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using FabricStore.Models;
+
+    class UowData : IUowData
+    {
+        private readonly DbContext context;
+        private readonly Dictionary<Type, object> repositories = new Dictionary<Type, object>();
+
+        public UowData()
+            : this(new ApplicationDbContext())
+        {
+        }
+
+        public UowData(DbContext context)
+        {
+            this.context = context;
+        }
+
+        private IRepository<T> GetRepository<T>() where T : class
+        {
+            if (!this.repositories.ContainsKey(typeof(T)))
+            {
+                var type = typeof(GenericRepository<T>);
+
+                this.repositories.Add(typeof(T), Activator.CreateInstance(type, this.context));
+            }
+
+            return (IRepository<T>)this.repositories[typeof(T)];
+        }
+
+        public int SaveChanges()
+        {
+            return this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+
+        public IRepository<Product> Products
+        {
+            get { return this.GetRepository<Product>(); }
+        }
+
+        public IRepository<Category> Categories
+        {
+            get { return this.GetRepository<Category>(); }
+        }
+
+        public IRepository<Manufacturer> Manufacturers
+        {
+            get { return this.GetRepository<Manufacturer>(); }
+        }
+
+        public IRepository<Comment> Comments
+        {
+            get { return this.GetRepository<Comment>(); }
+        }
+
+        public IRepository<Tag> Tags
+        {
+            get { return this.GetRepository<Tag>(); }
+        }
+    }
+}
