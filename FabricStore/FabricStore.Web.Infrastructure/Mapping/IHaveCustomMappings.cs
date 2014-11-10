86bcff29dd8dfae37b888ec95d056432be93198e
simoto <sime679@gmail.com>
2014-11-10 20:46:27 +0200

    added Infrastructure Library

---
+++ FabricStore/FabricStore.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace FabricStore.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
---
+++ FabricStore/FabricStore.Web.Infrastructure/Mapping/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace FabricStore.Web.Infrastructure.Mapping
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
