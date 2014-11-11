675757fe196ddce7098e3b946b59e761639833f8
simoto <sime679@gmail.com>
2014-11-12 00:25:13 +0200

    Added CategoriesController and change Products/Details.cshtml to have link with items with the same category

---
+++ FabricStore/FabricStore.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FabricStore.Data;
+using FabricStore.Models;
+using AutoMapper.QueryableExtensions;
+using FabricStore.Web.Models;
+
+namespace FabricStore.Web.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        private IRepository<Category> categories;
+
+        public CategoriesController(IRepository<Category> categories)
+        {
+            this.categories = categories;
+        }
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var allCategories = this.categories.All().Project().To<CategoryViewModel>();
+            return View(allCategories);
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var allCategoryProducts = this.categories.All()
+                .FirstOrDefault(x => x.Id == id).Products.AsQueryable().Project().To<ProductHomeViewModel>();
+
+            if (allCategoryProducts == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(allCategoryProducts);
+        }
+    }
+}
---
+++ FabricStore/FabricStore.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FabricStore.Web.Models
+{
+    using FabricStore.Models;
+    using FabricStore.Web.Infrastructure.Mapping;
+
+    public class CategoryViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
